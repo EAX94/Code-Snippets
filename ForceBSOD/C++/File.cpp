5110068a13eb373e5c6ceb2af8582c092ef30de2
KISKE <kiskegl@gmail.com>
2020-06-08 21:41:53 -0300

    Update File.cpp

--- ForceBSOD/C++/File.cpp
+++ ForceBSOD/C++/File.cpp
@@ -3,14 +3,14 @@
 #pragma comment(lib, "ntdll.lib")
 
 EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
-EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnocodeStringParameterMask, PULONG_PTR Parameters, ULONG ValidResponceOption, PULONG Responce);
+EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnocodeStringParameterMask, PULONG_PTR Parameters, ULONG ValidResponseOption, PULONG Response);
 
 int main() {
     BOOLEAN bl;
-    unsigned long responce;
+    unsigned long response;
 	
     RtlAdjustPrivilege(19, true, false, &bl);
-    NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, 0, 6, &responce);
+    NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, 0, 6, &response);
 
     return 0;
 }
